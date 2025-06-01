2e95d94d518036e436b022bf63b99cde28579bda
Rprasanth362k <prasanth362k@gmail.com>
2025-06-01 12:13:03 +0530

    commit by prasanth

--- Java_World/src/java_Method/Test_withMethod.java
+++ Java_World/src/java_Method/Test_withMethod.java
@@ -8,12 +8,31 @@ public class Test_withMethod {
 		int sum =a+b;
 		System.out.println("Sum: "+ sum);
 	}
+public static int minFunction(int n1, int n2)
+{
+	int min;
+//	System.out.println("Min value is : "+min); you can not initialize local variable  ,when printing came compile time error.
 
+	if (n1>n2)
+	{
+		min =n2;
+	System.out.println("Min:n2 is : "+min);
+	return min;
+	}
+			else
+			{	
+	min=n1;
+	System.out.println("Min:n1 is : "+min);
+	return min;
+			}
+	}
+
+	
 	public static void main(String[] args) {
 		
    add(5,3);// call method 
    add(10,2); // reuse method 
-
+   minFunction(1,2);
 	}
 
 }
