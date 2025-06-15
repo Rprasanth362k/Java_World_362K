1e827eec62e4111ee857f2a6eb355f92e3653c70
Rprasanth362k <prasanth9002k@gmail.com>
2025-06-16 02:27:16 +0530

    commit by prasanht

---
+++ Java_World/src/abstraction/Flower.java
@@ -0,0 +1,19 @@
+package abstraction;
+
+abstract public class Flower {
+
+	abstract public void fragent();
+	//abstract private void fragent();
+	abstract protected void madicineFlower();
+	abstract  void notEatableFlowers();
+	
+
+	
+
+	
+	public static void nonFragent()
+	{
+		
+	}
+
+}
